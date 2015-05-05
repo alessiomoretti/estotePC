0853f81173e15ae488730b1706dc6b3151e9883f
Alessio Moretti <alessio.moretti@live.it>
2015-05-05 19:47:34 +0200

    First proposal
    A simple sql script to create a new table useful for our project. The second query will be used to create a first row that then will be reused by the mysql server to store the data.

---
+++ SmartGreen/website/creation.sql
@@ -0,0 +1,9 @@
+CREATE TABLE measures( id INTEGER PRIMARY KEY NOT NULL,
+                       tempA             REAL NOT NULL,
+                       tempB             REAL NOT NULL,
+                       tempC             REAL NOT NULL,
+                       moistA            REAL NOT NULL,
+                       moistB            REAL NOT NULL,
+                       moistC            REAL NOT NULL );
+
+INSERT INTO measures VALUES(1, 0, 0, 0, 0, 0, 0);
