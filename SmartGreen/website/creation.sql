29e6a9769c54923fc10da84674b2001e3c7f6c93
Alessio Moretti <alessio.moretti@live.it>
2015-05-06 09:14:51 +0200

    Update - correct values

--- SmartGreen/website/creation.sql
+++ SmartGreen/website/creation.sql
@@ -2,8 +2,8 @@ CREATE TABLE measures( id INTEGER PRIMARY KEY NOT NULL,
                        tempA             REAL NOT NULL,
                        tempB             REAL NOT NULL,
                        tempC             REAL NOT NULL,
-                       moistA            REAL NOT NULL,
-                       moistB            REAL NOT NULL,
-                       moistC            REAL NOT NULL );
+                       moistA            VARCHAR(4) NOT NULL,
+                       moistB            VARCHAR(4) NOT NULL,
+                       moistC            VARCHAR(4) NOT NULL);
 
-INSERT INTO measures VALUES(1, 0, 0, 0, 0, 0, 0);
+INSERT INTO measures VALUES(1, 0.0, 0.0, 0.0, "T", "T", "T");
